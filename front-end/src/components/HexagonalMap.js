87f0357a60d94493b773cc4fb1af4058463e845f
Carl Wang <carlwang49@gmail.com>
2023-12-25 05:44:21 +0800

    set the player position

--- front-end/src/components/HexagonalMap.js
+++ front-end/src/components/HexagonalMap.js
@@ -1,20 +1,36 @@
-import React from 'react';
-import './components_css/HexagonalMap.css';
+import React from "react";
+import PlayerToken from "./PlayerToken";
+import "./components_css/HexagonalMap.css";
 
-function HexagonalMap() {
-  const hexagons = generateHexagonalGrid(6); // Radius of the large hexagon is 5
-  
-
+function HexagonalMap({ players }) {
+  const hexagons = generateHexagonalGrid(6); // Radius of the large hexagon is 6
+  console.log(hexagons);
   // Calculate the total width and height based on the hexagon size
-  const hexSize = Math.min((window.innerWidth * 0.9) / 11 / Math.sqrt(3), (window.innerHeight * 0.9) / 9 / 2);
+  const hexSize = Math.min(
+    (window.innerWidth * 0.9) / 11 / Math.sqrt(3),
+    (window.innerHeight * 0.9) / 9 / 2
+  );
   const totalWidth = Math.sqrt(3) * hexSize * 11 * 1.2; // Approximate width for 5 hexes + gaps
   const totalHeight = 2 * hexSize * 9 * 1.2; // Approximate height for 5 hexes + gaps
 
   return (
     <div className="hexagonal-map">
-      <svg viewBox={`-${totalWidth / 2} -${totalHeight / 2} ${totalWidth} ${totalHeight}`}>
+      <svg
+        viewBox={`-${totalWidth / 2} -${
+          totalHeight / 2
+        } ${totalWidth} ${totalHeight}`}
+      >
         {hexagons.map((hex, i) => (
           <Hexagon key={i} q={hex.q} r={hex.r} size={hexSize} />
+        ))}
+
+        {players.map((player, index) => (
+          <PlayerToken
+            key={player.id}
+            player={player}
+            hexSize={hexSize}
+            index={index}
+          />
         ))}
       </svg>
     </div>
@@ -25,41 +41,57 @@ export default HexagonalMap;
 
 function Hexagon({ q, r, size }) {
   // Convert axial to pixel coordinates
-  const x = size * Math.sqrt(3) * (q + r/2);
-  const y = size * 3/2 * r;
+  const x = size * Math.sqrt(3) * (q + r / 2);
+  const y = ((size * 3) / 2) * r;
 
   // Points for a flat-topped hexagon
   const hexPoints = [
-    { x: x + size * Math.sqrt(3) / 2, y: y - size / 2 },
-    { x: x + size * Math.sqrt(3) / 2, y: y + size / 2 },
+    { x: x + (size * Math.sqrt(3)) / 2, y: y - size / 2 },
+    { x: x + (size * Math.sqrt(3)) / 2, y: y + size / 2 },
     { x: x, y: y + size },
-    { x: x - size * Math.sqrt(3) / 2, y: y + size / 2 },
-    { x: x - size * Math.sqrt(3) / 2, y: y - size / 2 },
+    { x: x - (size * Math.sqrt(3)) / 2, y: y + size / 2 },
+    { x: x - (size * Math.sqrt(3)) / 2, y: y - size / 2 },
     { x: x, y: y - size },
   ];
 
   return (
     <polygon
-      points={hexPoints.map(p => `${p.x},${p.y}`).join(' ')}
+      points={hexPoints.map((p) => `${p.x},${p.y}`).join(" ")}
       className="hexagon"
     />
   );
 }
 
+/**
+ * Function to generate a hexagonal grid
+ * @param {number} radius - The maximum distance from the center to the edge hexagon
+ * @returns {Array} An array containing coordinates of edge hexagons
+ */
 function generateHexagonalGrid(radius) {
-    let hexagons = [];
-  
-    for (let q = -radius; q <= radius; q++) {
-      let r1 = Math.max(-radius, -q - radius);
-      let r2 = Math.min(radius, -q + radius);
-      for (let r = r1; r <= r2; r++) {
-        // Add only the hexagons that are on the edge
-        if (Math.abs(q) === radius || Math.abs(r) === radius || Math.abs(-q-r) === radius) {
-          hexagons.push({ q, r });
-        }
+  // Array to store the generated hexagon coordinates
+  let hexagons = [];
+
+  // Iterate over the q-axis of the hexagon coordinate system
+  for (let q = -radius; q <= radius; q++) {
+    // Calculate the range of the-axis in the hexagon coordinate system
+    let r1 = Math.max(-radius, -q - radius);
+    let r2 = Math.min(radius, -q + radius);
+
+    // Iterate over the r-axis of the hexagon coordinate system
+    for (let r = r1; r <= r2; r++) {
+      // Add only the coordinates of hexagons that are on the edge
+      if (
+        Math.abs(q) === radius ||
+        Math.abs(r) === radius ||
+        Math.abs(-q - r) === radius
+      ) {
+        hexagons.push({ q, r });
       }
     }
-  
-    return hexagons;
   }
-  
+
+  // Return an array containing coordinates of all edge hexagons
+  return hexagons;
+}
+
+
--- front-end/src/components/PlayerToken.js
+++ front-end/src/components/PlayerToken.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-function PlayerToken({ player }) {
-  // Render the player token based on the player's current position
+function PlayerToken({ player, hexSize, index }) {
+  // Convert axial to pixel coordinates
+  const x = hexSize * Math.sqrt(3) * (player.position.q + player.position.r / 2);
+  const y = hexSize * (3 / 2) * player.position.r;
+
+  // Add some offset based on the player's index
+  const offset = index * 5; // Adjust the value as needed
+  const adjustedX = x + offset;
+  const adjustedY = y + offset;
 
   return (
-    <div>
-      <h3>{player.name}'s Token</h3>
-      {/* Add your player token rendering logic here */}
-    </div>
+    <circle cx={adjustedX} cy={adjustedY} r={hexSize / 8} fill="red" />
   );
 }
 
